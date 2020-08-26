300e51c513c7e3920cdd140241bce33c2458b5fb
bitsyntex <bitsyntex@gmail.com>
2020-08-26 15:44:56 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+io.on('connection', function(socket) {
+  console.log('A new WebSocket connection has been established');
+});
+
+setInterval(function() {
+  var stockprice = Math.floor(Math.random() * 1000);
+  io.emit('stock price update', stockprice);
+}, 50);
+
+app.listen(8000, function() {
+  console.log('Listening on :8000');
+});
