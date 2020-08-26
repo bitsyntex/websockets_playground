ebf8a32ab89a9cbee2ea577da30aa5b16474312b
bitsyntex <bitsyntex@gmail.com>
2020-08-26 18:31:34 +0200

    fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer();
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 app.set('view engine', 'ejs');
@@ -14,11 +14,13 @@ io.on('connection', function(socket) {
   console.log('A new WebSocket connection has been established');
 });
 
+
 setInterval(function() {
   var stockprice = Math.floor(Math.random() * 1000);
   io.emit('stock price update', stockprice);
 }, 50);
 
-app.listen(8000, function() {
+
+server.listen(8000, function() {
   console.log('Listening on :8000');
 });
